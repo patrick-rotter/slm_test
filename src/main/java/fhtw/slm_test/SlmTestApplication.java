4e7602293a8318494f34b45e5537555446e3e437
Patrick Rotter <patrick.rotter@gmail.com>
2022-12-06 19:33:29 +0100

    Initial commit

---
+++ src/main/java/fhtw/slm_test/SlmTestApplication.java
@@ -0,0 +1,13 @@
+package fhtw.slm_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmTestApplication.class, args);
+	}
+
+}
