555405378d65f954403440f33cda5b77a854823e
Patrick Rotter <patrick.rotter@gmail.com>
2022-12-06 19:54:03 +0100

    Add inch to cm calc #3

--- src/main/java/fhtw/slm_test/controller/CalcController.java
+++ src/main/java/fhtw/slm_test/controller/CalcController.java
@@ -1,7 +1,14 @@
 package fhtw.slm_test.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalcController {
+
+    @GetMapping("/inchToCm/{num}")
+    Double calcCmFromInch(@PathVariable Double num) {
+        return num * 2.54;
+    }
 }
